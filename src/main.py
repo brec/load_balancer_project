d7b79136ae8323a091b4e68672853f5b7069f8d8
Brandon Recinos <brandon@Brandons-MacBook-Pro.local>
2025-02-18 21:58:20 -0700

    Reorganize source files add class definition skeletons.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+class LoadBalancer:
+    """A load balancer that distributes requets across multiple servers.
+    The LoadBalancer class implements a round robin traffic distribution
+    pattern for simple evaluation of load balancing patterns.
+    """
+
+    def __init__(self):
+        self.servers = []
+        pass
+
+    def add_server(self, server_address):
+        pass
+
+    def round_robin(self):
+        pass
