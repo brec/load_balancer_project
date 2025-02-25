e24bcd6bcbe9c34488bca8b045076793d1923f5f
Your Name <brec212@gmail.com>
2025-02-24 21:42:33 -0700

    restructure folders to improve package declaration

---
+++ load_balancer/__init__.py
@@ -0,0 +1,3 @@
+from .balancer import LoadBalancer
+
+__version__ = '0.1.0'
---
+++ main.py
@@ -0,0 +1,4 @@
+from load_balancer import LoadBalancer
+if __name__ == "__main__":
+    lb = LoadBalancer()
+    lb.run()
